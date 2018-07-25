a83f9ae7c73d8608e97a495183f0026a7885aed7
XUHO3 <howells.xu@oocl.com>
2018-07-25 14:50:32 +0800

    complete the addParkingboyToParkingLot function on ParkingLotsControllor

--- src/main/java/com/example/demo/controllor/ParkingLotsControllor.java
+++ src/main/java/com/example/demo/controllor/ParkingLotsControllor.java
@@ -4,10 +4,7 @@ import com.example.demo.model.ParkingLotModel;
 import com.example.demo.service.ParkingBoysService;
 import com.example.demo.service.ParkingLotsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,4 +28,8 @@ public class ParkingLotsControllor {
         return  parkingLotsService.addParkingLot(parkingLot);
     }
 
+    @PutMapping("parkinglots/parkinglot/{id}/parkingboy/{boyid}")
+     public  boolean  giveBoyidToParkingLot(@PathVariable int id,@PathVariable int boyid){
+        return parkingLotsService.giveBoyidToParkingLot(id,boyid);
+    }
 }
--- src/main/java/com/example/demo/service/ParkingLotsService.java
+++ src/main/java/com/example/demo/service/ParkingLotsService.java
@@ -1,11 +1,13 @@
 package com.example.demo.service;
 
 import com.example.demo.model.DbService;
+import com.example.demo.model.ParkingBoyModel;
 import com.example.demo.model.ParkingLotModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ParkingLotsService {
@@ -23,5 +25,22 @@ public class ParkingLotsService {
         List<ParkingLotModel> lotModelList=dbService.getLotList();
         lotModelList.add(parkingLot);
         return  parkingLot;
+    }
+
+    public boolean giveBoyidToParkingLot(int id, int boyid) {
+        List<ParkingLotModel> lotModelList=dbService.getLotList();
+        List<ParkingBoyModel> boyModelList=dbService.getBoysList();
+        List<ParkingBoyModel>newlist=  boyModelList.stream().filter(boy->boy.boyId==boyid).collect(Collectors.toList());
+        if(newlist.size()==0){
+            return false;
+        }
+        for(ParkingLotModel lot :lotModelList){
+            if(lot.lotId==id){
+                lot.setParkingBoy(newlist.get(0));
+                return true;
+            }
+        }
+        return false;
+
     }
 }
